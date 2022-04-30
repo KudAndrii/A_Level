d0058b35148a6eb1680ad74b8b6cfe8b33ea2977
Андрей Кудь <cud.andrei@gmail.com>
2022-04-30 11:42:57 +0300

    xml file added to class program

--- PractiseLessonThree/PractiseLessonThree/Program.cs
+++ PractiseLessonThree/PractiseLessonThree/Program.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace PractiseLessonThree
 {
+    /// <summary>
+    /// Point of Entering the program.
+    /// </summary>
     internal class Program
     {
         public static void Main(string[] args)
