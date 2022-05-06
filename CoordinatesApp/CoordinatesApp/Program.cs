a278432fe9aef234c2472f17afba540784881122
Андрей Кудь <cud.andrei@gmail.com>
2022-05-06 14:00:40 +0300

    Project created

---
+++ CoordinatesApp/CoordinatesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoordinatesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
