c78e24f946655042120b2c80d17a552cfc67c7ae
Андрей Кудь <cud.andrei@gmail.com>
2022-05-07 14:23:19 +0300

    Return projects

---
+++ HomeTaskLessonOne/HomeTaskLessonOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeTaskLessonOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Andrew";
+            string surname = "Kud";
+            int age = 28;
+            int toHundred = 100 - age;
+            Console.WriteLine($"{name} {surname}, you have {toHundred} years to 100");
+        }
+    }
+}
