4d3bb53138b18d5bad25a96f0456269550864784
Андрей Кудь <cud.andrei@gmail.com>
2022-05-16 21:33:09 +0300

    Work on ZooServices started

---
+++ Zoo/Zoo/ZooServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.AbstractAnimals;
+using Zoo.Enums;
+using Zoo.Models;
+
+namespace Zoo
+{
+    internal class ZooServices
+    {
+        public Creature[] GenerateZoo(int lenght)
+        {
+            Creature[] zoo = new Creature[lenght];
+            Random random = new Random();
+
+
+
+            return zoo;
+        }
+    }
+}
