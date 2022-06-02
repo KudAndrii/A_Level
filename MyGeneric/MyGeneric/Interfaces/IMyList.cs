80cfa1885215a7d327510d8096298c25f5bfebe2
Андрей Кудь <cud.andrei@gmail.com>
2022-06-02 23:10:38 +0300

    Work in process

---
+++ MyGeneric/MyGeneric/Interfaces/IMyList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGeneric.Interfaces
+{
+    public interface IMyList<T>
+    {
+        /// <summary>
+        /// Method adds specified element.
+        /// </summary>
+        /// <param name="element">Specified element witch should be added.</param>
+        public void Add(T element);
+
+        /// <summary>
+        /// Method adds collection of specified elements.
+        /// </summary>
+        /// <param name="elements">Collection of specified elements witch should be added.</param>
+        public void AddRange(T[] elements);
+
+        /// <summary>
+        /// Method removes first specified element.
+        /// </summary>
+        /// <param name="element">Specified element witch chould be removed.</param>
+        /// <param name="result">Result of removing (suссessful removal = true).</param>
+        public void Remove(T element, out bool result);
+
+        /// <summary>
+        /// Method removes element by specified index.
+        /// </summary>
+        /// <param name="index">Specified index.</param>
+        public void RemoveAt(int index);
+
+
+        public void Sort();
+    }
+}
---
+++ MyGeneric/MyGeneric/MyListServices/MyList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGeneric.Interfaces;
+
+namespace MyGeneric.MyListServices
+{
+    internal class MyList<T> : IMyList<T>
+    {
+        private const int STARTLENGTH = 16;
+        private T[] _list;
+        private int _emptyIndex;
+        public MyList()
+        {
+            _list = new T[STARTLENGTH];
+            _emptyIndex = 0;
+        }
+
+        public MyList(T item)
+            : this()
+        {
+            Add(item);
+        }
+
+        public MyList(T[] items)
+        {
+            if (items.Length > STARTLENGTH)
+            {
+                _list = new T[items.Length * 2];
+            }
+            else
+            {
+                _list = new T[STARTLENGTH];
+            }
+
+            AddRange(items);
+        }
+
+        public T[] List
+        {
+            get
+            {
+                return CopyIntoNewList(_emptyIndex);
+            }
+        }
+
+        public void Add(T element)
+        {
+            CheckLength(_emptyIndex);
+            _list[_emptyIndex] = element;
+            _emptyIndex++;
+        }
+
+        public void AddRange(T[] elements)
+        {
+            CheckLength(_emptyIndex + elements.Length);
+            for (int i = 0; i < elements.Length; i++)
+            {
+                _list[_emptyIndex + i] = elements[i];
+            }
+
+            _emptyIndex += elements.Length;
+        }
+
+        public void Remove(T element, out bool result)
+        {
+        }
+
+        public void RemoveAt(int index)
+        {
+        }
+
+        public void Sort()
+        {
+        }
+
+        private T[] CopyIntoNewList(int length)
+        {
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = _list[i];
+            }
+
+            return result;
+        }
+
+        private void CheckLength(int length)
+        {
+            if (_list.Length <= length)
+            {
+                _list = CopyIntoNewList(_list.Length * 2);
+            }
+        }
+    }
+}
