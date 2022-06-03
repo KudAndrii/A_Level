67adaf272d1eb829b9484766ac0101a3cb72b4fd
Андрей Кудь <cud.andrei@gmail.com>
2022-06-03 21:21:10 +0300

    Code for testing removed

--- MyGeneric/MyGeneric/Program.cs
+++ MyGeneric/MyGeneric/Program.cs
@@ -8,37 +8,6 @@ namespace MyGeneric
     {
         private static void Main(string[] args)
         {
-            MyList<int> list = new MyList<int>();
-            list.Add(0);
-            Show(list);
-            int[] array = new int[] { 4, 2, 3, 1 };
-            list.AddRange(array);
-            Show(list);
-            bool flag;
-            list.Remove(1, out flag);
-            Show(list, flag);
-            list.RemoveAt(3);
-            Show(list);
-            list.Sort();
-            Show(list);
-        }
-
-        private static void Show(MyList<int> list, bool flag = false)
-        {
-            foreach (var item in list)
-            {
-                Console.Write(item + "\t");
-            }
-
-            Console.WriteLine();
-            if (flag == false)
-            {
-                Console.WriteLine();
-            }
-            else
-            {
-                Console.WriteLine(flag);
-            }
         }
     }
 }
