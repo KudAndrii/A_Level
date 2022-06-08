d18b7c2eb1383f94345b872282a7110d656b39f6
Андрей Кудь <cud.andrei@gmail.com>
2022-06-08 17:06:38 +0300

    Lists of names and surnames instead of enums added to needed direction

--- PhoneBook/PhoneBook/Services/ContactsServices.cs
+++ PhoneBook/PhoneBook/Services/ContactsServices.cs
@@ -13,6 +13,39 @@ namespace PhoneBook.Services
     internal class ContactsServices : IContactsServices
     {
         private readonly Random _random;
+        private readonly List<string> _names = new List<string>()
+        {
+        "Андрей",
+        "Антон",
+        "Вася",
+        "Виктор",
+        "Петя",
+        "Павел",
+        "Дима",
+        "Денис",
+        "Гриша",
+        "Andrew",
+        "Anton",
+        "Vasya",
+        "Viktor",
+        "John",
+        "Jorj",
+        "Stiven",
+        "Stifler",
+        "Kate"
+        };
+        private readonly List<string> _surnames = new List<string>()
+        {
+        "Smit",
+        "Shmidt",
+        "Galager",
+        "Geller",
+        "Попов",
+        "Павлов",
+        "Климов",
+        "911"
+        };
+
         public ContactsServices(Random random)
         {
             _random = random;
@@ -22,18 +55,18 @@ namespace PhoneBook.Services
         {
             IContact[] contactsArray = new Contact[length];
 
-            // Random Name, based on existing corresponding enum.
+            // Random Name, based on existing corresponding array.
             string randomName;
 
-            // Random Surname, based on existing corresponding enum.
+            // Random Surname, based on existing corresponding array.
             string randomSurname;
 
             // Random phone number.
             int randomPhoneNumber;
             for (int i = 0; i < length; i++)
             {
-                randomName = ((Names)_random.Next(0, 18)).ToString();
-                randomSurname = ((Surnames)_random.Next(0, 9)).ToString();
+                randomName = _names[_random.Next(0, 18)];
+                randomSurname = _surnames[_random.Next(0, 8)];
                 randomPhoneNumber = _random.Next(600000000, 999999999);
                 contactsArray[i] = new Contact(randomName, randomSurname, randomPhoneNumber);
             }
