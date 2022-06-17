96cc1ff02abf34e94678a4861eaf97af300e80bc
Андрей Кудь <cud.andrei@gmail.com>
2022-06-17 19:47:55 +0300

    It looks better

--- Multithreading/Multithreading/Program.cs
+++ Multithreading/Multithreading/Program.cs
@@ -11,20 +11,27 @@ namespace Multithreading
         static void Main(string[] args)
         {
             var p = new Program();
+            Console.WriteLine("Before calling method in main");
             var result = p.ConcatAsync();
             Console.WriteLine(result.Result);
+            Console.WriteLine("After calling method in main");
         }
 
         public async Task<string> GetWordAsync(string path)
         {
-            return await Task.Run(() => File.ReadAllText(path));
+            Console.WriteLine("Start");
+            var result = await Task.Run(() => File.ReadAllText(path));
+            Thread.Sleep(2000);
+            Console.WriteLine("End");
+            return result;
         }
 
         public async Task<string> ConcatAsync()
         {
-            string hello = await GetWordAsync("../../../Hello.txt");
-            string world = await GetWordAsync("../../../World.txt");
-            return hello + " " + world + "!";
+            var hello = GetWordAsync("../../../Hello.txt");
+            Console.WriteLine("Second call");
+            var world = GetWordAsync("../../../World.txt");
+            return hello.Result + " " + world.Result + "!";
         }
     }
 }
