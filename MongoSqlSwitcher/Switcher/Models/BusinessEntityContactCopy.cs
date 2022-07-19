6e06872ecc6b32e26eee884ca6f98f5f4705f7c0
Андрей Кудь <cud.andrei@gmail.com>
2022-07-19 20:51:45 +0300

    Ready to check.

---
+++ MongoSqlSwitcher/Switcher/Models/BusinessEntityContactCopy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Switcher.Models
+{
+    public partial class BusinessEntityContactCopy
+    {
+        public int BusinessEntityId { get; set; }
+        public int PersonId { get; set; }
+        public int ContactTypeId { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ MongoSqlSwitcher/Switcher/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Switcher
+{
+    using DbConnectors;
+    using Models;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees;
+            using (var sqlDb = new AdventureWorks2019SqlContext())
+            {
+                var someEmployees = sqlDb.Employees.Select(x => x).Where(x => x.BusinessEntityId <= 10);
+                Employees = someEmployees.ToList();
+            }
+
+            var MongoDb = new AdventureWorks2019MongoConnection();
+
+            MongoDb.Employees.InsertManyAsync(Employees);
+        }
+    }
+}
