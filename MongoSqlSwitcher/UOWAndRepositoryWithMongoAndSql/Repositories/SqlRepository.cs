230a93b369ca67c6f4a9cf3f2746871db55fa12e
Андрей Кудь <cud.andrei@gmail.com>
2022-07-31 21:30:38 +0300

    Creating of project structure started.

---
+++ MongoSqlSwitcher/UOWAndRepositoryWithMongoAndSql/Repositories/SqlRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UOWAndRepositoryWithMongoAndSql.Repositories
+{
+    using Interfaces;
+    public class SqlRepository : DbRepository
+    {
+        public Task DeleteById<TModel>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TModel>> GetAll<TModel>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> GetById<TModel>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertMany<TModel>(IEnumerable<TModel> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertOne<TModel>(TModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOne<TModel>(TModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
