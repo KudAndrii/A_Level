e76d47c72b4eea07bb7cc8ecbff22b1cf15c8a4f
Андрей Кудь <cud.andrei@gmail.com>
2022-08-10 23:28:18 +0300

    Work on header started.

---
+++ react-store-sample/src/Components/HeaderComponent.tsx
@@ -0,0 +1,46 @@
+import headerLogo from "../Images/headerLogo.png";
+
+const HeaderComponent = (): JSX.Element => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand p-0" href="/">
+            <img src={headerLogo} alt="A.I." width="80em" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="index.html"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="places.html">
+                  Favourite places
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default HeaderComponent;
